953f4810684919319408d550f8fd06391ad3406f
Lsq-Frog <lai413633@outlook.com>
2016-12-15 15:40:07 +0800

    修改routers路径问题

--- routers/router.go
+++ routers/router.go
@@ -1,10 +1,11 @@
 package routers
 
 import (
-	"hello/controllers"
+	"note/controllers"
+
 	"github.com/astaxie/beego"
 )
 
 func init() {
-    beego.Router("/", &controllers.MainController{})
+	beego.Router("/", &controllers.MainController{})
 }
