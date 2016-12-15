efb5c14e76b527adee3a0932442b93ee0d2e6fcc
Lsq-Frog <lai413633@outlook.com>
2016-12-15 15:59:44 +0800

    修改配置文件build.sh

--- script/build.sh
+++ script/build.sh
@@ -3,7 +3,7 @@ set -x
 
 # build project
 
-CGO_ENABLED=0 GOOS=linux go build -ldflags "-s" -a -installsuffix cgo -o note || exit $?
+CGO_ENABLED=0 GOOS=linux go build -ldflags "-s" -a -installsuffix cgo -o mynote || exit $?
 
 #go build -o note  || exit $?
 
@@ -11,10 +11,10 @@ cd -
 
 rm -rf bin
 mkdir bin
-mv note ./bin
-cp -r ./views ./bin
-cp -r ./static ./bin
-cp -r ./conf ./bin
+mv mynote ./bin
+cp -r views ./bin
+cp -r static ./bin
+cp -r conf ./bin
 
 echo "Build ok"
 exit 0
