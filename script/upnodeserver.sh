d5534be64adb7c5242c8883143b7b014f2ed98db
Lsq-Frog <lai413633@outlook.com>
2016-12-22 20:59:38 +0800

    更新一键更新脚本

--- script/upnodeserver.sh
+++ script/upnodeserver.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 set -x
-bash ./build.sh
-docker build -t mynote:master ../
+
+# build project
+CGO_ENABLED=0 GOOS=linux go build -ldflags "-s" -a -installsuffix cgo -o mynote || exit $?
+
+rm -rf bin
+mkdir bin
+mv ./mynote ./bin
+cp -r ./views ./bin
+cp -r ./static ./bin
+cp -r ./conf ./bin
+
+# make image
+docker build -t mynote:master ./
+
+# updata server
 docker run -d -p 0.0.0.0:8080:8080 -name mynote mynote:master
+
 echo "updata ok"
 exit 0
